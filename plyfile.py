9e179473a64071536fa34bbc488da3e882c9997c
Darsh Ranjan <darsh.ranjan@here.com>
2015-03-28 14:57:55 -0700

    Fix project name in license header comment

--- plyfile.py
+++ plyfile.py
@@ -13,7 +13,8 @@
 #   General Public License for more details.
 #
 #   You should have received a copy of the GNU General Public License
-#   along with termcolors.  If not, see <http://www.gnu.org/licenses/>.
+#   along with python-plyfile.  If not, see 
+#       <http://www.gnu.org/licenses/>.
 
 from itertools import islice as _islice
 
