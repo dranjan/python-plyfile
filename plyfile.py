d9a8f4f0b7bf23767328c8e949b8100229aeba5f
Ahmet Levent Tasel <leventtasel@gmail.com>
2023-12-12 10:05:06 -0800

    half float support

--- plyfile.py
+++ plyfile.py
@@ -1479,6 +1479,8 @@ _data_type_relation = [
     ('int', 'i4'),
     ('uint32', 'u4'),
     ('uint', 'u4'),
+    ('float16', 'f2'),
+    ('half', 'f2'),
     ('float32', 'f4'),
     ('float', 'f4'),
     ('float64', 'f8'),
